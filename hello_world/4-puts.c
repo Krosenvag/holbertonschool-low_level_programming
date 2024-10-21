6d117241ac87d90eb74e43d39de9437ef2289b05
Krosenvag <herve.leguennec13@gmail.com>
2024-10-21 10:14:43 +0000

    4 refais et corriger

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
  * main - my first c programming
- * Description:
+ * Description: Cprogram
  * Return: 0
 */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
