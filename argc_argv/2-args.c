28831f37358e2d92d8bc37e27cd915e37d70b19f
Krosenvag <herve.leguennec13@gmail.com>
2024-11-12 13:41:13 +0000

    2 Write a program that prints all arguments it receives.
    All arguments should be printed, including the first one
    Only print one argument per line, ending with a new line

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @argc : number of command-line arguments
+ * @argv : string of argumenst passed to main fcn
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int index;
 
+	for (index = 0; index < argc; index++)
+	{
+		printf("%s\n", argv[index]);
+	}
+
+	return (0);
+}
